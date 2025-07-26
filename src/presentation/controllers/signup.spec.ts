bd39020c51705f0825b160107a59f65104891ffb
horodeski <geovana.horodeski06@gmail.com>
2025-07-26 17:45:26 -0300

    feat: adicionar testes para validação de campos obrigatórios no SignUpController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,7 +16,7 @@ describe("SignUp Controller ", () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError("name"))
   })
-  
+
   test("Should return 400 if no email is provided", () => {
     const sut = new SignUpController();
     const httpRequest = {
@@ -26,9 +26,39 @@ describe("SignUp Controller ", () => {
         passwordConfirmation: "1234"
       }
     }
-    
+
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError("email"))
+  })
+
+  test("Should return 400 if no password is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: "geovana.horodeski06@gmail.com",
+        passwordConfirmation: "1234",
+        name: "geovana"
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError("password"))
+  })
+
+    test("Should return 400 if no password confirmation is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: "geovana.horodeski06@gmail.com",
+        password: "1234",
+        name: "geovana"
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,16 @@
-import { MissingParamError } from "../erros/missing-param-error"
-import { badRequest } from "../helpers/http-helper"
-import { httpRequest, httpResponse } from "../protocols/http"
+import { MissingParamError } from "../erros/missing-param-error";
+import { badRequest } from "../helpers/http-helper";
+import { httpRequest, httpResponse } from "../protocols/http";
+import { Controller } from "../protocols/controller"; 
 
-export class SignUpController {
-  handle(httpRequest: httpRequest): httpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-  }
+export class SignUpController implements Controller {
+	handle(httpRequest: httpRequest): httpResponse {
+		const requiredFields = ["name", "email", "password", "passwordConfirmation"];
+
+		for (const field of requiredFields) {
+			if (!httpRequest.body[field]) {
+				return badRequest(new MissingParamError(field));
+			}
+		}
+	}
 }
