534a02394f3780899a6a1b9d7ceb9f99883e251f
horodeski <geovana.horodeski06@gmail.com>
2025-08-19 20:45:36 -0300

    refactor: reorganizar imports no SignUpController e adicionar arquivo de index para protocolos

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from "./signup";
-import { EmailValidator } from "../protocols/email-validator";
 import { MissingParamError, InvalidParamError, ServerError } from "../errors";
+import { EmailValidator } from "../protocols";
 
 interface sutTypes {
 	sut: SignUpController;
