29a6a04eb5768a02259a82747a68728a75fcd59e
horodeski <geovana.horodeski06@gmail.com>
2025-08-19 20:40:13 -0300

    refactor: adicionar tratamento de erro no SignUpController utilizando função serverError

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
-import { httpResponse } from "../protocols/http"
+import { ServerError } from "../errors/server-error";
+import { httpResponse } from "../protocols/http";
 
 export const badRequest = (error: Error): httpResponse => ({
-  statusCode: 400,
-  body: error
-})
+	statusCode: 400,
+	body: error
+});
+
+export const serverError = (): httpResponse => ({
+	statusCode: 500,
+	body: new ServerError()
+});
