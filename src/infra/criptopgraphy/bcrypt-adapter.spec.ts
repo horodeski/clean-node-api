8c520c28790bd233bc4e3a6e46ec83eacf856ac3
horodeski <geovana.horodeski06@gmail.com>
2025-08-31 20:31:16 -0300

    refactor: simplificar a criação do BcryptAdapter e melhorar a legibilidade dos testes

--- src/infra/criptopgraphy/bcrypt-adapter.spec.ts
+++ src/infra/criptopgraphy/bcrypt-adapter.spec.ts
@@ -2,17 +2,21 @@ import bcrypt from "bcrypt";
 
 import {BcryptAdapter} from "./bcrypt-adapter";
 
+const salt = 12;
+
 jest.mock("bcrypt", () => ({
 	async hash(): Promise<string> {
-		return new Promise(resolve => resolve("hash"))
+		return new Promise((resolve) => resolve("hash"));
 	}
-}))
+}));
 
+const makeSut = (): BcryptAdapter => {
+	return new BcryptAdapter(salt);
+};
 
 describe("Bcrypt Adapter", () => {
 	test("Should call bcrypt with correct values", async () => {
-		const salt = 12;
-		const sut = new BcryptAdapter(salt);
+		const sut = makeSut();
 		const hashSpy = jest.spyOn(bcrypt, "hash");
 
 		await sut.encrypt("any_value");
@@ -20,8 +24,7 @@ describe("Bcrypt Adapter", () => {
 	});
 
 	test("Should return a hash on success", async () => {
-		const salt = 12;
-		const sut = new BcryptAdapter(salt);
+		const sut = makeSut();
 
 		const hash = await sut.encrypt("any_value");
 		expect(hash).toBe("hash");
